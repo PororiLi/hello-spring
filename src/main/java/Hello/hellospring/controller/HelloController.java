d403498ff729f9b1d0f52d53a5b4a12deea4f6e0
PororiLi <xiozioll@gmail.com>
2021-06-10 18:00:02 +0900

    API type

--- src/main/java/Hello/hellospring/controller/HelloController.java
+++ src/main/java/Hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -18,4 +19,34 @@ public class HelloController {
         model.addAttribute("name", name);
         return "hello-template";
     }
+
+    @GetMapping("hello-string")
+    @ResponseBody   // http 헤더와 바디 부의 바디. body 부분에 내가 직접 넣어주겠다. API 방식. 데이터가 직접 가는 것.
+    public String helloString(@RequestParam("name") String name) {
+        return "hello "+name;   //"hello Spring" 템플릿 엔진과의 차이는 뷰 없이 문자가 바로 가는 것.
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+
+
+        }
+    }
+
+
 }
