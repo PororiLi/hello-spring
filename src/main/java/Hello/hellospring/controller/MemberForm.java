b82a71a6097f904eab7575701eb02519c00c4ec9
PororiLi <xiozioll@gmail.com>
2021-06-14 11:44:01 +0900

    members new

---
+++ src/main/java/Hello/hellospring/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package Hello.hellospring.controller;
+
+public class MemberForm {
+    private String name;    //html에 작성한 input name ="name"을 보고 여기에 데이터 넣어줌.
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
