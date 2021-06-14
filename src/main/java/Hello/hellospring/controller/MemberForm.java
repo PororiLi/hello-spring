62fb074c9bca7be297deac7053dd2e6fce5a6ab2
PororiLi <xiozioll@gmail.com>
2021-06-14 16:33:31 +0900

    Add view memberlist

--- src/main/java/Hello/hellospring/controller/MemberForm.java
+++ src/main/java/Hello/hellospring/controller/MemberForm.java
@@ -1,6 +1,6 @@
 package Hello.hellospring.controller;
 
-public class MemberForm {
+public class MemberForm {   //DTO 클래스
     private String name;    //html에 작성한 input name ="name"을 보고 여기에 데이터 넣어줌.
 
     public String getName() {
