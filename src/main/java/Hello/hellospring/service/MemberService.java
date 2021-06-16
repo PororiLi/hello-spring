5669f9ea3e522683b1b0dc7cd9dbc54686106b6b
PororiLi <xiozioll@gmail.com>
2021-06-16 10:38:34 +0900

    Reason to use AOP

--- src/main/java/Hello/hellospring/service/MemberService.java
+++ src/main/java/Hello/hellospring/service/MemberService.java
@@ -25,11 +25,19 @@ public class MemberService {
     * */
     public Long join(Member member) {
         //같은 이름이 있는 중복 회원 x.
+        long start = System.currentTimeMillis();
+
+        try {
+            validateDuplicateMember(member);    // 중복 회원 검증.
+            memberRepo.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println(timeMs);
+        }
 
 
-        validateDuplicateMember(member);    // 중복 회원 검증.
-        memberRepo.save(member);
-        return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
@@ -50,7 +58,14 @@ public class MemberService {
     *
     * */
     public List<Member> findMembers() {
-        return memberRepo.findAll();
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepo.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println(timeMs);
+        }
     }
 
     public Optional<Member> findOne(Long memberId) {
